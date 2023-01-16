bf2b9cbc46c588605505c92a5d613304c3f1ce33
ppcf_ <ppcf_@DESKTOP-H39U3KR>
2023-01-16 13:33:23 +0100

    Cuarto commit: Creación ENUM Opcion
    Creación de clase tipo ENUM con las distintas opciones.

---
+++ src/main/java/org/iesalandalus/programacion/clientesempresa/vista/Opcion.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.clientesempresa.vista;
+
+public enum Opcion {
+	
+	BORRAR_CLIENTE, BUSCAR_CLIENTE, INSERTAR_CLIENTE, MOSTRAR_CLIENTES, MOSTRAR_CLIENTES_FECHA, SALIR;
+
+}
